4dca5817338ac3e59720d09587801e024c8642fe
Simon Anguish <simonanguish@mac.com>
2016-03-17 13:58:19 -0400

    Woring Highest weighted scheduling algorithm. Does not return the schedule, but returns the highest weight possible

--- Java/Highest_Weighted_Scheduling/Highest_Weighted_Scheduling.java
+++ Java/Highest_Weighted_Scheduling/Highest_Weighted_Scheduling.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Highest_Weighted_Scheduling {
 	static Schedule s = buildSchedule();
+	static int m[] = new int[s.size()];
+	static int p[] = new int[s.size()];
 	public static void main(String[] args) {
 		HighestWeightedScheduling();
 	}
@@ -10,7 +12,7 @@ class Highest_Weighted_Scheduling {
 		Schedule s = new Schedule();
 		
 		s.addJob(new Job(9, 1.1, 1.2));
-		s.addJob(new Job(14,1.3, 7.9));
+		s.addJob(new Job(1, 1.3, 7.9));
 		s.addJob(new Job(2, 1.45,6.08));
 		s.addJob(new Job(2, 7.3, 9.0));
 		s.addJob(new Job(2, 4.06,4.08));
@@ -34,10 +36,29 @@ class Highest_Weighted_Scheduling {
 		System.out.println(s.toString());
 		
 		// p-values for the ith job
-		int p[] = new int[s.size()];
+//		int p[] = new int[s.size()];
 		for (int i=0;i<s.size();i++) {
 			p[i] = p(i);
 //			System.out.printf("i: %d\tp(i): %d\n", i, p[i]);
 		}
+		
+		// Opt[]
+//		int m[] = new int[s.size()];
+		for (int i=0;i<m.length;i++) {
+			m[i] = -1;
+		}
+		
+		m[0] = 0;
+		
+		int max = Compute_Opt(s.size() - 1);
+		System.out.println(max);
+	}
+	
+	private static int Compute_Opt(int j) {
+		if (m[j] == -1) {
+			m[j] = (s.getJob(j).getWeight() + Compute_Opt(p[j]) <= Compute_Opt(j-1) ? Compute_Opt(j-1) : s.getJob(j).getWeight() + Compute_Opt(p[j]));
+		}
+		
+		return m[j];
 	}
 }
