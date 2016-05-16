45c269658e5bb02e1034037f78a8109a7097e373
Simon Anguish <simonanguish@mac.com>
2016-05-16 16:33:03 -0400

    Added Elise folder

---
+++ Java/Elise/Elise.java
@@ -0,0 +1,15 @@
+class Elise {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Vocabulary {
+	private ArrayList<int> = new ArrayList<int>();
+	
+	public Vocabulary() {}
+	
+	public void addWord(String word) {
+		
+	}
+}
